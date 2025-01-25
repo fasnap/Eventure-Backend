04dd6cab0feda3d3fa9142ce834fcf306957b88d
fasnap <fasnapkdy@gmail.com>
2025-01-26 02:30:34 +0530

    Removed commented logging code from settings

--- eventure/settings.py
+++ eventure/settings.py
@@ -267,24 +267,6 @@ CHANNEL_LAYERS = {
     }
 }
 
-
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'console': {
-#             'class': 'logging.StreamHandler',
-#         },
-#     },
-#     'loggers': {
-#         'django.db.backends': {
-#             'handlers': ['console'],
-#             'level': 'DEBUG',
-#         },
-#     },
-# }
-
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
